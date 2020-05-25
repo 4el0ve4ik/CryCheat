f97a3b040892f1a5647b7f044d149b2245dc3bce
4el0ve4ik <aask7806@gmail.com>
2020-05-25 22:04:11 +0300

    update hook d3d10

--- DirectXHook/D3D10Hook.cpp
+++ DirectXHook/D3D10Hook.cpp
@@ -1,49 +1,84 @@
 #include "D3D10Hook.h"
+#include <string>
 
-namespace D3D10HK {
+D3D10HOOK::D3D10HOOK(HWND hWnd) {
+	if (!initializedHook) {
 
-	ID3D10Device * pDevice = NULL;
-	IDXGISwapChain * pSwapChain = NULL;
-	bool initializedHook = false;
 
-	bool InitiateHook(HWND hWnd,void* hkfunc, IDXGISwapChain__Present *oFunc)
-	{
-		if (!initializedHook) {
-			D3D_FEATURE_LEVEL featureLevel = D3D_FEATURE_LEVEL_10_0;
-			DXGI_SWAP_CHAIN_DESC sd;
-			ZeroMemory(&sd, sizeof(sd));
-			sd.BufferCount = 2;
-			sd.BufferDesc.Width = 0;
-			sd.BufferDesc.Height = 0;
-			sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-			sd.BufferDesc.RefreshRate.Numerator = 60;
-			sd.BufferDesc.RefreshRate.Denominator = 1;
-			sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
-			sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-			sd.OutputWindow = hWnd;
-			sd.SampleDesc.Count = 1;
-			sd.SampleDesc.Quality = 0;
-			sd.Windowed = TRUE;
-			sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+		D3D_FEATURE_LEVEL featureLevel = D3D_FEATURE_LEVEL_10_0;
+		DXGI_SWAP_CHAIN_DESC sd;
+		ZeroMemory(&sd, sizeof(sd));
+		sd.BufferCount = 2;
+		sd.BufferDesc.Width = 0;
+		sd.BufferDesc.Height = 0;
+		sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		sd.BufferDesc.RefreshRate.Numerator = 60;
+		sd.BufferDesc.RefreshRate.Denominator = 1;
+		sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+		sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+		sd.OutputWindow = hWnd;
+		sd.SampleDesc.Count = 1;
+		sd.SampleDesc.Quality = 0;
+		sd.Windowed = TRUE;
+		sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+		UINT createDeviceFlags = 0;
+		createDeviceFlags |= D3D10_CREATE_DEVICE_DEBUG;
+		if (D3D10CreateDeviceAndSwapChain(NULL, D3D10_DRIVER_TYPE_HARDWARE, NULL, createDeviceFlags, D3D10_SDK_VERSION, &sd, &pSwapChain, &pDevice) == S_OK) {
+			SwapChainVtable = *(void***)pSwapChain;
 
-			UINT createDeviceFlags = 0;
-			createDeviceFlags |= D3D10_CREATE_DEVICE_DEBUG;
-			if (D3D10CreateDeviceAndSwapChain(NULL, D3D10_DRIVER_TYPE_HARDWARE, NULL, createDeviceFlags, D3D10_SDK_VERSION, &sd, &pSwapChain, &pDevice) != S_OK)
-				return false;
+			DeviceVtable = *(void***)pDevice;
 
-			DWORD oldProtection;
-			void ** SwapChainVtable = *(void ***)pSwapChain;
-			VirtualProtect(&SwapChainVtable[8], 4, PAGE_EXECUTE_READWRITE, &oldProtection);
+			for (unsigned char i = 0; i <= 97; i++)
+				oDeviceFunc[i] = 0;
+			for (unsigned char i = 0; i <= 17; i++)
+				oSwapChainFunc[i] = 0;
 
-			*oFunc = (IDXGISwapChain__Present)SwapChainVtable[8];
-
-			SwapChainVtable[8] = hkfunc;
-			VirtualProtect(&SwapChainVtable[8], 4, oldProtection, NULL);
-			pSwapChain->Release();
-			pDevice->Release();
 			initializedHook = true;
 		}
-		return true;
 	}
-	
+}
+
+D3D10HOOK::~D3D10HOOK() {
+	for (unsigned char i = 0; i <= 97; i++) {
+		if (oDeviceFunc[i] != 0)
+			DeviceVtable[i] = (void*)oDeviceFunc[i];
+	}
+	for (unsigned char i = 0; i <= 17; i++) {
+		if (oSwapChainFunc[i] != 0)
+			SwapChainVtable[i] = (void*)oSwapChainFunc[i];
+	}
+	pDevice->Release();
+	pSwapChain->Release();
+}
+
+void D3D10HOOK::SetSwapChainHook(void* Func, SwapChainIndex index) {
+	if (oSwapChainFunc[index] == 0) {
+		DWORD oldProtection;
+		VirtualProtect(&SwapChainVtable[index], 4, PAGE_EXECUTE_READWRITE, &oldProtection);
+		oSwapChainFunc[index] = (unsigned long long)SwapChainVtable[index];
+		SwapChainVtable[index] = Func;
+		VirtualProtect(&SwapChainVtable[index], 4, oldProtection, NULL);
+	}
+}
+
+void D3D10HOOK::SetDeviceHook(void* Func, DeviceIndex index) {
+	if (oDeviceFunc[index] == 0) {
+		unsigned long oldProt;
+		VirtualProtect((void*)&DeviceVtable[index], 4, PAGE_EXECUTE_READWRITE, &oldProt);
+		oDeviceFunc[index] = (unsigned long long)DeviceVtable[index];
+		DeviceVtable[index] = Func;
+		VirtualProtect((void*)&DeviceVtable[index], 4, oldProt, NULL);
+	}
+}
+
+long long D3D10HOOK::GetOriginDeviceFunc(DeviceIndex index) {
+	return oDeviceFunc[index];
+}
+
+long long D3D10HOOK::GetOriginSwapChainFunc(SwapChainIndex index) {
+	return oSwapChainFunc[index];
+}
+
+bool D3D10HOOK::GetHookStatus() {
+	return initializedHook;
 }
--- DirectXHook/D3D10Hook.h
+++ DirectXHook/D3D10Hook.h
@@ -6,9 +6,145 @@
 #pragma comment(lib, "C:/Program Files (x86)/Microsoft DirectX SDK (June 2010)/Lib/x64/d3d10.lib")
 #pragma comment(lib, "C:/Program Files (x86)/Microsoft DirectX SDK (June 2010)/Lib/x64/d3dx10.lib")
 
-namespace D3D10HK
+enum SwapChainIndex
 {
-	typedef HRESULT(__fastcall * IDXGISwapChain__Present)(IDXGISwapChain * pSwapChain, UINT SyncInterval, UINT Flags);
-	bool InitiateHook(HWND hWnd, void* hkfunc, IDXGISwapChain__Present *oFunc);
-}
+	iQueryInterface = 0,
+	iAddRef,
+	iRelease,
+	iSetPrivateData,
+	iSetPrivateDataInterface,
+	iGetPrivateData,
+	iGetParent,
+	iGetDevice,
+	iPresent,
+	iGetBuffer,
+	iSetFullscreenState,
+	iGetFullscreenState,
+	iGetDesc,
+	iResizeBuffers,
+	iResizeTarget,
+	iGetContainingOutput,
+	iGetFrameStatistics,
+	iGetLastPresentCount
+};
 
+enum DeviceIndex {
+	iiQueryInterface = 0,
+	iiAddRef,
+	iiRelease,
+	iVSSetConstantBuffers,
+	iPSSetShaderResources,
+	iPSSetShader,
+	iPSSetSamplers,
+	iVSSetShader,
+	iDrawIndexed,
+	iDraw,
+	iPSSetConstantBuffers,
+	iIASetInputLayout,
+	iIASetVertexBuffers,
+	iIASetIndexBuffer,
+	iDrawIndexedInstanced,
+	iDrawInstanced,
+	iGSSetConstantBuffers,
+	iGSSetShader,
+	iIASetPrimitiveTopology,
+	iVSSetShaderResources,
+	iVSSetSamplers,
+	iSetPredication,
+	iGSSetShaderResources,
+	iGSSetSamplers,
+	iOMSetRenderTargets,
+	iOMSetBlendState,
+	iOMSetDepthStencilState,
+	iSOSetTargets,
+	iDrawAuto,
+	iRSSetState,
+	iRSSetViewports,
+	iRSSetScissorRects,
+	iCopySubresourceRegion,
+	iCopyResource,
+	iUpdateSubresource,
+	iClearRenderTargetView,
+	iClearDepthStencilView,
+	iGenerateMips,
+	iResolveSubresource,
+	iVSGetConstantBuffers,
+	iPSGetShaderResources,
+	iPSGetShader,
+	iPSGetSamplers,
+	iVSGetShader,
+	iPSGetConstantBuffers,
+	iIAGetInputLayout,
+	iIAGetVertexBuffers,
+	iIAGetIndexBuffer,
+	iGSGetConstantBuffers,
+	iGSGetShader,
+	iIAGetPrimitiveTopology,
+	iVSGetShaderResources,
+	iVSGetSamplers,
+	iGetPredication,
+	iGSGetShaderResources,
+	iGSGetSamplers,
+	iOMGetRenderTargets,
+	iOMGetBlendState,
+	iOMGetDepthStencilState,
+	iSOGetTargets,
+	iRSGetState,
+	iRSGetViewports,
+	iRSGetScissorRects,
+	iGetDeviceRemovedReason,
+	iSetExceptionMode,
+	iGetExceptionMode,
+	iiGetPrivateData,
+	iiSetPrivateData,
+	iiSetPrivateDataInterface,
+	iClearState,
+	iFlush,
+	iCreateBuffer,
+	iCreateTexture1D,
+	iCreateTexture2D,
+	iCreateTexture3D,
+	iCreateShaderResourceView,
+	iCreateRenderTargetView,
+	iCreateDepthStencilView,
+	iCreateInputLayout,
+	iCreateVertexShader,
+	iCreateGeometryShader,
+	iCreateGeometryShaderWithStreamOutput,
+	iCreatePixelShader,
+	iCreateBlendState,
+	iCreateDepthStencilState,
+	iCreateRasterizerState,
+	iCreateSamplerState,
+	iCreateQuery,
+	iCreatePredicate,
+	iCreateCounter,
+	iCheckFormatSupport,
+	iCheckMultisampleQualityLevels,
+	iCheckCounterInfo,
+	iCheckCounter,
+	iGetCreationFlags,
+	iOpenSharedResource,
+	iSetTextFilterSize,
+	iGetTextFilterSize
+};
+
+class D3D10HOOK {
+public:
+	D3D10HOOK(HWND hWnd);
+	~D3D10HOOK();
+	void SetSwapChainHook(void* Func, SwapChainIndex index);
+	void SetDeviceHook(void* Func, DeviceIndex index);
+	long long GetOriginDeviceFunc(DeviceIndex index);
+	long long GetOriginSwapChainFunc(SwapChainIndex index);
+	bool GetHookStatus();
+
+private:
+	ID3D10Device * pDevice = NULL;
+	IDXGISwapChain * pSwapChain = NULL;
+	void** SwapChainVtable;
+	void** DeviceVtable;
+	bool initializedHook = false;
+	unsigned long long oSwapChainFunc[17];
+	unsigned long long oDeviceFunc[97];
+};
